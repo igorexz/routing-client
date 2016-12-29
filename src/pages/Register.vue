638a4eaf050f2003b00c9827ecf632f27cbc061f
banana <banana@localhost.localdomain>
2016-12-30 00:00:56 +0200

    first commit

---
+++ src/pages/Register.vue
@@ -0,0 +1,8 @@
+<template>
+	<div id="register">
+    Login: <br><input type="text" name="login"><br><br>
+    Password: <br><input type="text" name="password"><br><br>
+    Email: <br><input type="text" name="email"><br><br>
+    <input type="submit" onclick="window.location.href='/training'" class="submit">
+    </div>
+</template>
